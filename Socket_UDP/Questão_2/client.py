2e8156bb3e3b3e9a95e1f510cf148ccc277d58e7
Shinohara <julianoKS1999@hotmail.com>
2022-09-27 02:49:05 -0300

    Atualização UDP
    Co-authored-by: Gaby <GabrielaMarangoni@users.noreply.github.com>

--- Socket_UDP/Questão_2/client.py
+++ Socket_UDP/Questão_2/client.py
@@ -1,12 +1,16 @@
-################################################################################################################# 
-# Questão 2 - UDP CLIENT                                                                                        #                                                                              #
-# Descrição: 
-# 
-#   
-# Autores: Gabriela Marangoni Radigonda e Juliano Kendyi Shinohara                                              #
-# Data de criação: 23/09/2022                                                                                   #
-# Data de modificação: 25/09/2022 
-#################################################################################################################
+#################################################################################################################### 
+# Questão 2 - UDP CLIENT                                                                                           #                                                                              #
+# Descrição: Fazer um sistema de upload de arquivos via UDP. Um servidor UDP deverá receber as partes dos arquivos #
+#(1024 bytes), verificar ao final a integridade via um checksum (SHA-1) e armazenar o arquivo em uma pasta padrão. #
+#Sugestões: o servidor pode receber o nome e tamanho do arquivo como o primeiro pacote e o checksum como o último. #
+#Testar o servidor com arquivos textos e binários (ex: imagens, pdf) de tamanhos arbitrários (ex: 100 bytes, 4KiB, #
+#4MiB). O protocolo para a comunicação deve ser criado e especificado textualmente ou graficamente.                #
+#                                                                                                                  #
+# Autores: Gabriela Marangoni Radigonda e Juliano Kendyi Shinohara                                                 #
+# Data de criação: 23/09/2022                                                                                      #
+# Data de modificação: 25/09/2022                                                                                  #  
+#                      26/09/2022                                                                                  #    
+####################################################################################################################
 import socket
 import os
 import math
@@ -16,7 +20,7 @@ HOST = "localhost"
 PORT = 6000
 addr = (HOST, PORT)
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)  
 clientSocket.connect(addr)
 
@@ -29,34 +33,35 @@ def main():
 
         if(file.__contains__(fileName)):
             if (aux < 256):
-                fileSize = int.to_bytes((os.stat('./files' + fileName).st_size), 4, 'big')
+                fileSize = int.to_bytes((os.stat('./archiveClient/' + fileName).st_size), 4, 'big')
                 qtdFile = math.ceil(aux / 1024) # faz a divisão por 1024 para saber em quantas partes o arquivo será enviado 
                 clientSocket.send(fileSize)
-                openFile = open('./files/' + fileName,'rb')  # leitura binário
+                openFile = open('./archiveClient/' + fileName,'rb')  # leitura binário
+
                 # faz o checksum (soma de verificação) no arquivo para verifica-lo
                 checksum = openFile.read()
-                md5_hash = hashlib.md5()
-                md5_hash.update(checksum)
-                checksum = md5_hash.hexdigest()
+                sha1_hash = hashlib.sha1(checksum)               
+                checksum = sha1_hash.hexdigest() # hexdigest retorna como um objeto string de comprimento duplo, contendo apenas dígitos hexadecimais
                # checksum = hashlib.sha1(openFile.read()).hexdigest() # faz o checksum (soma de verificação) no arquivo para verifica-lo
                 
-                file = fileName + str(fileSize) + checksum + str(qtdFile)
-                clientSocket.sendto(file.encode(), addr)
+                # file = fileName + str(fileSize) + checksum + str(qtdFile)
+                # print('file: ', file)
+                # clientSocket.sendto(file.encode(), addr)
 
                 openFile.seek(0) # seek é usado para que abra o arquivo para ler desde o inicio, já que tivemos que dividir o arquivo em pedaços
                 
-                while openFile.read(1024) != b'':
+                for _ in range(qtdFile):
                     clientSocket.sendto(openFile.read(1024), addr) #envia para o servidor
-                    openFile.close() 
+                    print('addr: ', addr)
 
+                print('checksum: ', checksum)
                 clientSocket.sendto(checksum.encode(), addr)
-            
+                openFile.close() 
+
             else: print('ERROR!')
         
         else: print('There is no file here with this name!')
                 
-        clientSocket.send(fileName) 
-
-               
+                     
 if __name__ == "__main__":
     main()
--- Socket_UDP/Questão_2/server.py
+++ Socket_UDP/Questão_2/server.py
@@ -1,12 +1,16 @@
-######################################################################################### 
-# Questão 2 - UDP CLIENT                                                                #                                                                              #
-# Descrição:                                                                            #
-#   Servidor recebe um arquivo do cliente e verifica se o checksum é identico ao local. #   
-#                                                                                       #   
-# Autores: Gabriela Marangoni Radigonda e Juliano Kendyi Shinohara                      #
-# Data de criação: 23/09/2022                                                           #
-# Data de modificação: 25/09/2022                                                       #
-#########################################################################################
+#################################################################################################################### 
+# Questão 2 - UDP SERVIDOR                                                                                           #                                                                              #
+# Descrição: Fazer um sistema de upload de arquivos via UDP. Um servidor UDP deverá receber as partes dos arquivos #
+#(1024 bytes), verificar ao final a integridade via um checksum (SHA-1) e armazenar o arquivo em uma pasta padrão. #
+#Sugestões: o servidor pode receber o nome e tamanho do arquivo como o primeiro pacote e o checksum como o último. #
+#Testar o servidor com arquivos textos e binários (ex: imagens, pdf) de tamanhos arbitrários (ex: 100 bytes, 4KiB, #
+#4MiB). O protocolo para a comunicação deve ser criado e especificado textualmente ou graficamente.                #
+#                                                                                                                  #
+# Autores: Gabriela Marangoni Radigonda e Juliano Kendyi Shinohara                                                 #
+# Data de criação: 23/09/2022                                                                                      #
+# Data de modificação: 25/09/2022                                                                                  #
+#                      26/09/2022                                                                                  #    
+####################################################################################################################
 
 import math
 import socket
@@ -19,7 +23,7 @@ PORT = 6000
 addr = (HOST, PORT)
 
 # Cria socket e define a instancia
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serverSocket.bind(addr)
 
@@ -32,13 +36,13 @@ def main():
     while True:
         requestClient, addr = serverSocket.recvfrom(1024)
         dados = {'userIP': addr[0], 'userPort': addr[1]}
-        fileSize = int.from_bytes(requestClient, addr = serverSocket.recvfrom(1024) [:4], byteorder = 'big')
+        fileSize = int.from_bytes(requestClient[:4], byteorder = 'big')
         fileName = requestClient[4:].decode()
         qtyPackets = math.ceil(fileSize/1024)
         nameLog.info('Protocol: %s', 'Downloading...', extra=dados)
         file = open('./archiveServer/' + fileName, 'w+b')
 
-        for i in range(qtyPackets):
+        for _ in range(qtyPackets):
             inf, addr = serverSocket.recvfrom(1024)
             file.write(inf)
 
