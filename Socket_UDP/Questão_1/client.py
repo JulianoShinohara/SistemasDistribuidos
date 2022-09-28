8be8785a6596d46e522d07bc46c05f06a8399327
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-09-27 23:39:31 -0300

    testando

--- Socket_UDP/Questão_1/client.py
+++ Socket_UDP/Questão_1/client.py
@@ -80,7 +80,7 @@ def receiveMessage(HOST, PORT):
                 print(nickname + ' >> ' + messageReceive)
             
             case 2: #emoji
-                print(nickname + ' >> ' + emoji.emojize(messageReceive))
+                print(nickname + ' >> Emoji: ' + emoji.emojize(messageReceive))
 
             case 3: #URL
                 print(nickname + ' >> Link: ' + messageReceive)
@@ -92,13 +92,7 @@ def receiveMessage(HOST, PORT):
          
 
 def urlMessage(url):
-    regexp = re.compile(
-        r'^(?:http|ftp)s?://' 
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' 
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    regexp = re.compile('^(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:/?|[/?]\S+)$', re.IGNORECASE)
     return re.match(regexp, url)
 
 def main():
